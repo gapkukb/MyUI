5c5ed324cf3e9a81a125f8610bac72cd1f7de21c
Carlos <Carlos@tmg66.com>
2021-03-01 18:02:30 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-                <Button>发链接连接按时付款就类似的看法距离首都里的精神分裂的凯撒附件胜多负少</Button>
+                <Button block harline loading>发链接连接按时付款就类似的看法距离首都里的精神分裂的凯撒附件胜多负少</Button>
 				{factory()}
 				{factory({ disabled: true })}
 				{factory({ icon: "bars", href: "", round: true })}
--- src/package/button/index.tsx
+++ src/package/button/index.tsx
@@ -8,106 +8,107 @@ import Progress, { LodingProps } from "../loading";
 import { colors } from "../../json/vars.json";
 
 export type ButtonProps = AnchorHTMLAttributes<HTMLElement> &
-  Omit<ButtonHTMLAttributes<HTMLElement>, "type"> &
-  Partial<{
-    bgcolor: string;
-    block: boolean;
-    circle: boolean;
-    className: string;
-    color: string;
-    disabled: boolean;
-    ghost: boolean;
-    harline: boolean;
-    height: Numeric;
-    icon: string;
-    iconRight: string;
-    iconRightSlot: ReactNode;
-    iconSlot: ReactNode;
-    label: string;
-    loading: boolean;
-    loadingIcon: LodingProps["type"];
-    nativeType: "button" | "submit" | "reset" | "image";
-    outline: boolean;
-    replace: boolean;
-    round: boolean;
-    size: "normal" | "large" | "small" | "mini";
-    style: CSSProperties;
-    to: string;
-    type: "primary" | "danger" | "default" | "success" | "info" | "warning" | "link" | "dark" | "light"
-    width: Numeric;
-  }>;
+	Omit<ButtonHTMLAttributes<HTMLElement>, "type"> &
+	Partial<{
+		bgcolor: string;
+		block: boolean;
+		circle: boolean;
+		className: string;
+		color: string;
+		disabled: boolean;
+		ghost: boolean;
+		harline: boolean;
+		height: Numeric;
+		icon: string;
+		iconRight: string;
+		iconRightSlot: ReactNode;
+		iconSlot: ReactNode;
+		label: string;
+		loading: boolean;
+		loadingIcon: LodingProps["type"];
+		nativeType: "button" | "submit" | "reset" | "image";
+		outline: boolean;
+		replace: boolean;
+		radius: Spacing | "round" | "circle";
+		size: "normal" | "large" | "small" | "mini";
+		style: CSSProperties;
+		to: string;
+		type: "primary" | "danger" | "default" | "success" | "info" | "warning" | "link" | "dark" | "light";
+		width: Numeric;
+	}>;
 
 export const Button: FC<ButtonProps> = ({
-  bgcolor,
-  block,
-  children,
-  circle,
-  className,
-  color,
-  disabled,
-  ghost,
-  harline,
-  height,
-  href,
-  icon,
-  iconRight,
-  iconRightSlot,
-  iconSlot,
-  label,
-  loading,
-  loadingIcon,
-  nativeType = "button",
-  outline,
-  round,
-  size = "normal",
-  style,
-  to,
-  type,
-  width,
-  ...rest
+	bgcolor,
+	block,
+	children,
+	circle,
+	className,
+	color,
+	disabled,
+	ghost,
+	harline,
+	height,
+	href,
+	icon,
+	iconRight,
+	iconRightSlot,
+	iconSlot,
+	label,
+	loading,
+	loadingIcon,
+	nativeType = "button",
+	outline,
+	radius,
+	size = "normal",
+	style,
+	to,
+	type,
+	width,
+	...rest
 }: ButtonProps) => {
-  const Tag = to !== undefined ? "link" : href !== undefined ? "a" : "button";
-  const isButtonNode = Tag === "button";
-  const props: Record<any, any> = {
-    className: classnames("button", type, size, className, {
-      disabled: !isButtonNode && disabled,
-      outline,
-      harline,
-      round,
-      block,
-      circle,
-      ghost,
-    }),
-    style: Object.assign(
-      {
-        width: fixUnit(width),
-        height: fixUnit(height),
-        color,
-        backgroundColor: bgcolor,
-      },
-      style
-    ),
-    disabled: disabled && isButtonNode,
-    to,
-    href,
-    type: nativeType,
-  };
-  const text = children || label;
+	const Tag = to !== undefined ? "link" : href !== undefined ? "a" : "button";
+	const isButtonNode = Tag === "button";
+	const props: Record<any, any> = {
+		className: classnames("button", type, size, className, {
+			disabled: !isButtonNode && disabled,
+			outline,
+			harline,
+			["radius--" + radius]: radius,
+			block,
+			circle,
+			loading,
+		}),
+		style: Object.assign(
+			{
+				width: fixUnit(width),
+				height: fixUnit(height),
+				color,
+				backgroundColor: bgcolor,
+			},
+			style
+		),
+		disabled: isButtonNode && (disabled || loading),
+	};
+	if (Tag == "link") props.to = to;
+	else if (Tag == "a") props.href = href;
+	else if (Tag == "button") props.type = nativeType;
 
-  return (
-    <Tag {...props} {...rest}>
-      {loading && <Progress type={loadingIcon} />}
-      {iconSlot || (icon && <Icon name={icon} className="button__text" />)}
-      {text && <span className="button__text">{text}</span>}
-      {iconRightSlot || (iconRight && <Icon name={iconRight} className="button__text" />)}
-    </Tag>
-  );
+	const text = children || label;
+	const innerClassname = classnames("button__text", { button__loading: loading });
+	return (
+		<Tag {...props} {...rest}>
+			{loading && <Progress type={loadingIcon} />}
+			{iconSlot || (icon && <Icon name={icon} className="button__text" />)}
+			{text && <span className={innerClassname}>{text}</span>}
+			{iconRightSlot || (iconRight && <Icon name={iconRight} className={innerClassname} />)}
+		</Tag>
+	);
 };
 
 Button.defaultProps = {
-  type: "default",
-  nativeType: "button",
-  size: "normal",
+	type: "default",
+	nativeType: "button",
+	size: "normal",
 };
 
 export default Button;
