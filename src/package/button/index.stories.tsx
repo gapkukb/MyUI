8614a55cc1f16a283b4b4e82dc6108cdb52736e3
carlos <carlos>
2021-03-08 09:02:53 +0800

    update

--- src/package/button/index.stories.tsx
+++ src/package/button/index.stories.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Button, ButtonProps } from "./index";
+const arr: ButtonProps["type"][] = ["default", "danger", "primary", "success", "warning", "info", "light", "dark"];
+const sizes: ButtonProps["size"][] = ["large", "normal", "small", "mini"];
+const types: ButtonProps["type"][] = ["default", "danger", "dark", "info", "light", "primary", "success", "warning"];
+const colors: ButtonProps["color"][] = ["gold", "#5a1513", "rgba(146,94,93,0.5)"];
+const bgcolors: ButtonProps["bgcolor"][] = ["gold", "#5a1513", "rgba(146,94,93,0.5)", "to right, #ff6034, #ee0a24"];
 
 var defaults = {
 	title: "Button",
@@ -32,14 +37,113 @@ export const IconColor = () => {
 	};
 	return (
 		<>
-            
-			{factory()}
-			{factory({ disabled: true })}
-			{factory({ icon: "bars", href: "", round: true })}
-			{factory({ icon: "bars", iconRight: "archive" })}
-			{factory({ icon: "bars", label: "", circle: true })}
-			{factory({ icon: "bars", label: "", loading: true, loadingIcon: "clock" })}
-			{factory({ ghost: true })}
+			<div>
+				<div>
+					<span>实心：</span>
+					{types.map((type) => (
+						<Button type={type}>{type}</Button>
+					))}
+				</div>
+				<div>
+					<span>空心：</span>
+					{types.map((type) => (
+						<Button type={type} hollow>
+							{type}
+						</Button>
+					))}
+				</div>
+				<div>
+					<span>尺寸</span>
+					{sizes.map((size) => (
+						<Button size={size}>{size}</Button>
+					))}
+					<Button width="300" height="60">
+						通过width和height控制具体尺寸
+					</Button>
+				</div>
+				<div>
+					<span>禁用</span>
+					{types.map((type) => (
+						<Button type={type} disabled>
+							{type}
+						</Button>
+					))}
+					<Button width="300" height="60">
+						通过width和height控制具体尺寸
+					</Button>
+				</div>
+				<div>
+					<span>导航守卫</span>
+					<Button>URL导航</Button>
+					<Button>路由导航</Button>
+				</div>
+				<Button block>块级按钮 block</Button>
+				<div>
+					<span>自定义颜色：</span>
+					{colors.map((color) => (
+						<>
+							<Button color={color}>{color}</Button>
+							<Button color={color} hollow>
+								{color}
+							</Button>
+						</>
+					))}
+				</div>
+				<div>
+					<span>自定义背景色,支持渐变：</span>
+					{bgcolors.map((color) => (
+						<Button bgcolor={color} hollow>
+							{color}
+						</Button>
+					))}
+				</div>
+				<div>
+					<span>图标</span>
+					<Button icon="wifi" iconRight="wifi">
+						图标按钮
+					</Button>
+					<Button icon="wifi"></Button>
+				</div>
+				<div>
+					<span>圆角</span>
+					<Button>圆角按钮</Button>
+					<Button round="no">圆角按钮</Button>
+					<Button round="xl">圆角按钮</Button>
+					<Button round="lg">圆角按钮</Button>
+					<Button round="md">圆角按钮</Button>
+					<Button round="sm">圆角按钮</Button>
+					<Button round="xs">圆角按钮</Button>
+					<Button round="pill">圆角按钮</Button>
+					<Button round="circle">圆角按钮</Button>
+				</div>
+				<div>
+					<span>细边框</span>
+					<Button hollow round="no">
+						圆角按钮
+					</Button>
+					<Button hollow round="xl">
+						圆角按钮
+					</Button>
+					<Button hollow round="lg">
+						圆角按钮
+					</Button>
+					<Button hollow round="md">
+						圆角按钮
+					</Button>
+					<Button hollow round="sm">
+						圆角按钮
+					</Button>
+					<Button hollow round="xs">
+						圆角按钮
+					</Button>
+					<Button hollow round="pill">
+						圆角按钮
+					</Button>
+					<Button hollow round="circle">
+						圆角按钮
+					</Button>
+				</div>
+			</div>
 		</>
 	);
 };
--- src/package/field/index.tsx
+++ src/package/field/index.tsx
@@ -1,23 +1,108 @@
-import { FC, CSSProperties } from "react";
-  import classnames from "classnames";
-  import "./index.styl";
-  export type FieldProps = {
-    className: string,
-    style: CSSProperties,
-  };
-  
-  export const Field: FC<FieldProps> = ({ className, style, children, ...rest }) => {
-    function clickHandler(){}
-    const props = {
-      className: classnames("field",{
-  
-      }),
-      style:{
-        ...style
-      },
-      onClick:clickHandler,
-      ...rest,
-    };
-    return <div {...props}>{children}</div>;
-  };
-  export default Field;
+import { FC, CSSProperties, ReactNode, HTMLAttributes, useState, FormEvent, InputHTMLAttributes } from "react";
+import classnames from "classnames";
+import "./index.styl";
+type Align = "left" | "right" | "center";
+export type FieldProps = Partial<{
+	value: string;
+	autoFocus: boolean;
+	border: boolean;
+	button: ReactNode;
+	className: string;
+	clearable: boolean;
+	colon: boolean;
+	disabled: boolean;
+	error: ReactNode;
+	formatter: Function;
+	icon: string;
+	id: string;
+	inputAligh: Align;
+	label: ReactNode;
+	labelAligh: Align;
+	labelClassName: string;
+	labelWidth: Numeric;
+	leftIcon: string;
+	maxLength: Numeric;
+	name: string;
+	onBlur: (e: FormEvent<HTMLInputElement>) => void;
+	onChange: (e: FormEvent<HTMLInputElement>) => void;
+	onClear: (e: FormEvent<HTMLInputElement>) => void;
+	onFocus: (e: FormEvent<HTMLInputElement>) => void;
+	onClick: (e: MouseEvent) => void;
+	onIconClick: (e: MouseEvent) => void;
+	onInputClick: (e: MouseEvent) => void;
+	onLeftIconClick: (e: MouseEvent) => void;
+	onRightIconClick: (e: MouseEvent) => void;
+	placeholder: string;
+	prefix: ReactNode;
+	readonly: boolean;
+	required: boolean;
+	rightIcon: string;
+	showLimit: boolean;
+	size: "small" | "normal" | "large";
+	style: CSSProperties;
+	success: ReactNode;
+	suffix: ReactNode;
+	type: "text" | "number" | "integer" | "tel" | "email" | "password" | "url" | "search";
+}>;
+
+export const Field = ({
+	value = "",
+	autoFocus,
+	border,
+	button,
+	className,
+	clearable,
+	colon,
+	disabled,
+	error,
+	formatter = () => true,
+	icon,
+	id,
+	inputAligh,
+	label,
+	labelAligh,
+	labelClassName,
+	labelWidth,
+	leftIcon,
+	maxLength,
+	name,
+	onBlur,
+	onChange,
+	onClear,
+	onClick,
+	onFocus,
+	onIconClick,
+	onInputClick,
+	onLeftIconClick,
+	onRightIconClick,
+	placeholder,
+	prefix,
+	readonly,
+	required,
+	rightIcon,
+	showLimit,
+	size,
+	style,
+	success,
+	suffix,
+	type = "text",
+}: FieldProps) => {
+	const [focus, setFocus] = useState(false);
+	function changeHandler(e: FormEvent<HTMLInputElement>) {
+		const inputValue = e.currentTarget.value;
+		if (formatter(inputValue)) {
+			if (!maxLength) return onChange?.(e);
+			else if ((inputValue && inputValue.length < maxLength) || value.length < maxLength) {
+			}
+		}
+	}
+
+	const props = {
+		className: classnames("field", {}),
+		style: {
+			...style,
+		},
+	};
+	return <div {...props}></div>;
+};
+export default Field;
