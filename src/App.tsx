40ac5fad66899ec5b984166450d9eefd07ee5919
Carlos <Carlos@tmg66.com>
2021-03-08 18:13:18 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import Button from "./package/button";
 import Field from "./package/field";
+import Icon from "./package/icon";
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<Field></Field>
+				<Field
+                    colon
+					placeholder="完整结构"
+					type="text"
+					icon={<Icon name="phone"></Icon>}
+					rightIcon="wifi"
+                    label="完整结构"
+                    labelAlign="right"
+                    // error="错误信息"
+					button={<Button size="small">按钮</Button>}
+				></Field>
+				<Field colon label="请输入任意字符 :" placeholder="请输入任意字符" type="text" maxLength="6"></Field>
+				<Field colon label="请输入数字 :" placeholder="请输入数字" type="number" maxLength="6"></Field>
+				<Field colon label="请输入整数 :" placeholder="请输入整数" type="integer" maxLength="6"></Field>
+				<Field colon label="请输入邮箱 :" placeholder="请输入邮箱" type="email" maxLength="6"></Field>
+				<Field colon label="请输入搜索内容 :" placeholder="请输入搜索内容" type="search" maxLength="6"></Field>
+				<Field colon label="请输入url地址 :" placeholder="请输入url地址" type="url" maxLength="6"></Field>
+				<Field colon label="请输入电话 :" placeholder="请输入电话" type="tel" maxLength="6"></Field>
+				<Field colon label="请输入密码 :" placeholder="请输入密码" type="password" maxLength="6"></Field>
+				<Field colon label="文本输入域 :" placeholder="文本输入域" type="textarea" maxLength="6"></Field>
 			</div>
 		);
 	}
--- src/package/field/index.tsx
+++ src/package/field/index.tsx
@@ -1,31 +1,35 @@
-import { FC, CSSProperties, ReactNode, HTMLAttributes, useState, FormEvent, InputHTMLAttributes } from "react";
+import { FC, CSSProperties, ReactNode, HTMLAttributes, useState, FormEvent, InputHTMLAttributes, useRef } from "react";
 import classnames from "classnames";
 import "./index.styl";
+import Icon from "../icon";
+import { fixUnit } from "../../util/unit";
+import { nope } from "../../util";
 type Align = "left" | "right" | "center";
 export type FieldProps = Partial<{
 	value: string;
 	autoFocus: boolean;
 	border: boolean;
 	button: ReactNode;
+	center: boolean;
 	className: string;
 	clearable: boolean;
 	colon: boolean;
 	disabled: boolean;
 	error: ReactNode;
 	formatter: Function;
-	icon: string;
+	icon: ReactNode;
 	id: string;
-	inputAligh: Align;
+	inputAlign: Align;
 	label: ReactNode;
-	labelAligh: Align;
+	labelAlign: Align;
 	labelClassName: string;
 	labelWidth: Numeric;
-	leftIcon: string;
 	maxLength: Numeric;
+	messageAlign: Align;
 	name: string;
 	onBlur: (e: FormEvent<HTMLInputElement>) => void;
 	onChange: (e: FormEvent<HTMLInputElement>) => void;
-	onClear: (e: FormEvent<HTMLInputElement>) => void;
+	onClear: (el: HTMLInputElement) => void;
 	onFocus: (e: FormEvent<HTMLInputElement>) => void;
 	onClick: (e: MouseEvent) => void;
 	onIconClick: (e: MouseEvent) => void;
@@ -34,60 +38,64 @@ export type FieldProps = Partial<{
 	onRightIconClick: (e: MouseEvent) => void;
 	placeholder: string;
 	prefix: ReactNode;
-	readonly: boolean;
+	readOnly: boolean;
 	required: boolean;
-	rightIcon: string;
+	rightIcon: ReactNode;
 	showLimit: boolean;
 	size: "small" | "normal" | "large";
 	style: CSSProperties;
 	success: ReactNode;
 	suffix: ReactNode;
-	type: "text" | "number" | "integer" | "tel" | "email" | "password" | "url" | "search";
+	type: "text" | "number" | "integer" | "tel" | "email" | "password" | "url" | "search" | "textarea";
 }>;
 
 export const Field = ({
 	value = "",
 	autoFocus,
-	border,
+	border = true,
 	button,
+	center,
 	className,
-	clearable,
+	clearable = true,
 	colon,
 	disabled,
 	error,
 	formatter = () => true,
 	icon,
 	id,
-	inputAligh,
+	inputAlign = "left",
 	label,
-	labelAligh,
+	labelAlign = "left",
 	labelClassName,
 	labelWidth,
-	leftIcon,
 	maxLength,
+	messageAlign = "left",
 	name,
-	onBlur,
-	onChange,
-	onClear,
-	onClick,
-	onFocus,
-	onIconClick,
-	onInputClick,
-	onLeftIconClick,
-	onRightIconClick,
+	onBlur = nope,
+	onChange = nope,
+	onClear = nope,
+	onClick = nope,
+	onFocus = nope,
+	onIconClick = nope,
+	onInputClick = nope,
+	onLeftIconClick = nope,
+	onRightIconClick = nope,
 	placeholder,
 	prefix,
-	readonly,
+	readOnly,
 	required,
 	rightIcon,
 	showLimit,
-	size,
+	size = "normal",
 	style,
 	success,
 	suffix,
 	type = "text",
 }: FieldProps) => {
 	const [focus, setFocus] = useState(false);
+	const [model, setModel] = useState(value);
+	const fieldRef = useRef<HTMLInputElement>(null);
+
 	function changeHandler(e: FormEvent<HTMLInputElement>) {
 		const inputValue = e.currentTarget.value;
 		if (formatter(inputValue)) {
@@ -96,13 +104,117 @@ export const Field = ({
 			}
 		}
 	}
+	function blurHandler(e: FormEvent<HTMLInputElement>) {
+		const inputValue = e.currentTarget.value;
+		if (formatter(inputValue)) {
+			if (!maxLength) return onChange?.(e);
+			else if ((inputValue && inputValue.length < maxLength) || value.length < maxLength) {
+			}
+		}
+	}
+	function focusHandler(e: FormEvent<HTMLInputElement>) {
+		const inputValue = e.currentTarget.value;
+		if (formatter(inputValue)) {
+			if (!maxLength) return onChange?.(e);
+			else if ((inputValue && inputValue.length < maxLength) || value.length < maxLength) {
+			}
+		}
+	}
+	function inputClickHandler(e: FormEvent<HTMLInputElement>) {
+		const inputValue = e.currentTarget.value;
+		if (formatter(inputValue)) {
+			if (!maxLength) return onChange?.(e);
+			else if ((inputValue && inputValue.length < maxLength) || value.length < maxLength) {
+			}
+		}
+	}
+	function inputHandler(e: FormEvent<HTMLInputElement>) {
+        const value = e.currentTarget.value;
+        
+		setModel(value);
+	}
+	function clearHandler(e: FormEvent<HTMLInputElement>) {
+		fieldRef.current!.value = "";
+		fieldRef.current!.focus();
+		setModel("");
+		onClear(fieldRef.current!);
+	}
 
 	const props = {
-		className: classnames("field", {}),
+		className: classnames("field", size, { center: center || button }),
 		style: {
 			...style,
 		},
 	};
-	return <div {...props}></div>;
+	const labelProps = {};
+	const inputProps = {
+		type,
+		className: classnames("field__value", inputAlign),
+		value: value || undefined,
+		placeholder,
+		disabled,
+		readOnly,
+		maxLength: maxLength as number,
+		name,
+		autoFocus: autoFocus,
+		ref: fieldRef,
+		onChange: changeHandler,
+		onBlur: blurHandler,
+		onFocus: focusHandler,
+		onInput: inputHandler,
+		onClick: inputClickHandler,
+	};
+	if (type === "integer") {
+		Object.assign(inputProps, {
+			type: "tel",
+			inputMode: "numeric",
+		});
+	} else if (type === "number") {
+		Object.assign(inputProps, {
+			type: "text",
+			inputMode: "decimal",
+		});
+	}
+	const Tag = type === "textarea" ? "textarea" : ("input" as any);
+
+	function genIcon(input: typeof icon, classname: string, handler: Function) {
+		if (!icon) return undefined;
+		const props = {
+			className: classnames("field__icon", "field__icon--" + classname),
+			onClick: handler,
+		} as any;
+		return (
+			<div {...props}>{typeof input === "string" ? <Icon name={input} className={classname}></Icon> : icon}</div>
+		);
+	}
+	function genLabel() {
+		if (typeof label === "string")
+			return (
+				<div
+					className={classnames("field__label", labelAlign, labelClassName)}
+					style={{ width: fixUnit(labelWidth) }}
+				>
+					{label}
+				</div>
+			);
+		return icon;
+	}
+	return (
+		<div {...props}>
+			{genIcon(icon, "left", onLeftIconClick)}
+			{genLabel()}
+			{colon && <div className="field__colon">：</div>}
+			<div className="field__body">
+				<div className="field__main">
+					<Tag {...inputProps} />
+					{clearable && model && genIcon("times-circle", "clear", clearHandler)}
+					{button && <div className="field__button">{button}</div>}
+					{genIcon(rightIcon, "right", onRightIconClick)}
+				</div>
+				{error && <div className={classnames("field__message error", messageAlign)}>{error}</div>}
+				{success && <div className={classnames("field__message success", messageAlign)}>{success}</div>}
+			</div>
+		</div>
+	);
 };
 export default Field;
